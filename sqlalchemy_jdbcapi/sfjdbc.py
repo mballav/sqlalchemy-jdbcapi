f0bb4520be83c9bd67758aa768f11fd1b02bca4f
Manish Ballav <mballav@gmail.com>
2021-08-15 14:12:44 +0530

    Bug fix.

--- sqlalchemy_jdbcapi/sfjdbc.py
+++ sqlalchemy_jdbcapi/sfjdbc.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 import os
 import re
 
+from urllib.parse import unquote
+
 from sqlalchemy.sql import sqltypes
 from sqlalchemy import util, exc
 from sqlalchemy_jdbcapi.base import MixedBinary, BaseDialect
@@ -12,7 +14,16 @@ from sqlalchemy.engine.default import DefaultDialect
 
 class SalesforceJDBCDialect(BaseDialect, DefaultDialect):
     jdbc_db_name = "salesforce"
+
+    # this is the classname for open-source jdbc driver from dbschema:
+    # https://github.com/wise-coders/salesforce-jdbc-driver/
+    #
     jdbc_driver_name = "com.dbschema.salesforce.SalesforceJdbcDriver"
+
+    # Uncomment this line and comment the line above if you are using
+    # the jdbc driver from CData. URL that you use with SQLAlchemy
+    # remains the same for both the drivers.
+    #jdbc_driver_name = "cdata.jdbc.salesforce.SalesforceDriver"
 
     def initialize(self, connection):
         super(SalesforceJDBCDialect, self).initialize(connection)
@@ -24,18 +35,11 @@ class SalesforceJDBCDialect(BaseDialect, DefaultDialect):
         if url is None:
             return
         # dialects expect jdbc url e.g.
-        # "jdbc:oracle:thin@example.com:1521/db"
-        # if sqlalchemy create_engine() url is passed e.g.
-        # "oracle://scott:tiger@example.com/db"
-        # it is parsed wrong
+        # "jdbc:salesforce:User=<username>;Password=<password>;Security Token=<token>"
         # restore original url
         s: str = str(url)
         # get jdbc url
-        jdbc_url: str = s.split("//", 1)[-1]
-
-        # schema_url = 'jdbc:calcite:schemaFactory=com.jdreamer.driver.web.WebDataSchemaFactory;case_sensitive=false;schema.name=test;schema.url=http://{}/datalake/;schema.tenantId=1;schema.catalog=demo'.format(jdbc_url)
-
-        print(jdbc_url)
+        jdbc_url: str = unquote(s.split("//", 1)[-1])
 
         # add driver information
         kwargs = {
